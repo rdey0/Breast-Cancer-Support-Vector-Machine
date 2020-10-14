51fc6cd129d442e28c5a31ddc3d23ad1a532dd9d
Roy Dey <roydey@ucdavis.edu>
2020-10-13 22:47:54 -0700

    make foundation of front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import graph_img from './graph_img/graph.jpg'
 
 function App() {
+  
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="App-header">
+        <h1>Breast Cancer SVM</h1>
+      </div>
+      
+      <div className='gui-container'>
+        <div className='svm-display'>
+          <img  src={graph_img} alt='graph'/>
+        </div>
+        <div className='controls'>
+            <div className='slide-container'>
+              <input id="degree_slider" className="slider" type="range" min="1" max="10" defaultValue='1'/>
+            </div>
+            <div className='slide-container'>
+              <input id="cost_slider" className="slider" type="range" min="1" max="20" defaultValue='1'/>
+            </div>
+        </div>
+      </div>
+     
     </div>
   );
 }
