71e2f060a3a8d06a02b9d07be6f3103b1ace8740
Roy Dey <roydey@ucdavis.edu>
2020-10-27 16:57:57 -0700

    add onboarding modal header butons and styles

--- client/src/components/onboarding.js
+++ client/src/components/onboarding.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 
 
-const customStyles = {
+const modal_styles = {
     overlay: {
-        backgroundColor: 'rgba(65, 64, 64, 0.75)'
+        backgroundColor: 'rgba(136, 136, 136, 0.7)'
     },
     content:{
         height: '60%',
@@ -13,14 +13,19 @@ const customStyles = {
         top: '15%',
         left: '20%',
         border: 'none',
-        boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
+        borderRadius: '10px',
+        boxShadow: 'rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px'
     }
 };
 export default class Onboarding extends React.Component {
-    state={};
+
     constructor(props) {
         super();
-        this.state = {modal_open: props.showModal};
+        this.state = {
+            modal_open: props.showModal, 
+            step_num: 1, 
+            total_steps: 1
+        };
         this.close_modal = this.close_modal.bind(this);
     }
     
@@ -30,11 +35,17 @@ export default class Onboarding extends React.Component {
 
     render() {
         return(
-            <Modal isOpen={this.state.modal_open} style={customStyles}>
+            <Modal isOpen={this.state.modal_open} style={modal_styles}>
                 <div className='onboarding-controls'>
-                    <div onClick={this.close_modal}>Exit</div>
+                    <div className='step-container'>
+                        <div>Step {this.state.step_num} / {this.state.total_steps}</div>
+                    </div>
+                    <div className='change-page-container'>
+                        <div class='change-page-back'>Back</div>
+                        <div class='change-page-next'>Next</div>
+                    </div>
                 </div>
-                <div>
+                <div class-Name='onboarding-content'>
 
                 </div>
             </Modal>
